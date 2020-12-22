114ce9c0947350160c808f7c6e45365a914dba94
Igor <igor.rh@gmail.com>
2020-12-21 23:05:48 -0300

    Teste

---
+++ lib/src/home_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('List Todos\'s'),
+      ),
+      body: ListView.builder(
+          itemCount: 10,
+          itemBuilder: (context, index) {
+            return ListTile(title: Text("Item $index"));
+          }),
+    );
+  }
+}
